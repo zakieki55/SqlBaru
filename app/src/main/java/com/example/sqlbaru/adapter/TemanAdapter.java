eecc46a233e91b7db41f2725861fe19c678d85a6
zakieki55 <zakueki@gmail.com>
2022-04-24 13:04:19 +0700

    first commit

---
+++ app/src/main/java/com/example/sqlbaru/adapter/TemanAdapter.java
@@ -0,0 +1,78 @@
+package com.example.sqlbaru.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.sqlbaru.MainActivity;
+import com.example.sqlbaru.R;
+import com.example.sqlbaru.database.Teman;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class TemanAdapter extends RecyclerView.Adapter<TemanAdapter.TemanViewHolder> {
+
+    private ArrayList<Teman> listData;
+    private Context control;
+
+    public TemanAdapter(ArrayList<Teman> listData) {
+        this.listData = listData;
+    }
+
+    @Override
+    public TemanViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater layoutInf = LayoutInflater.from(parent.getContext());
+        View view = layoutInf.inflate(R.layout.row_data_teman, parent, false);
+        return new  TemanViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(TemanAdapter.TemanViewHolder holder, int position) {
+        String nm, tlp;
+
+
+        nm = listData.get(position).getNama();
+        tlp = listData.get(position).getTelpon();
+
+        holder.namaTxt.setTextColor(Color.BLUE);
+        holder.namaTxt.setTextSize(30);
+        holder.namaTxt.setText(nm);
+        holder.telponTxt.setText(tlp);
+
+        holder.cardku.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return (listData != null) ? listData.size() : 0;
+    }
+
+    public class TemanViewHolder extends RecyclerView.ViewHolder {
+
+        private CardView cardku;
+        private TextView namaTxt,telponTxt;
+
+        public TemanViewHolder(View view) {
+            super(view);
+
+            cardku =(CardView) view.findViewById(R.id.cardku);
+            namaTxt = (TextView) view.findViewById(R.id.textNama);
+            telponTxt = (TextView) view.findViewById(R.id.textTelpon);
+        }
+    }
+}
